44365b6e99bb1e475583fb26d998c7a8dc48cb81
Felip3FL <felip3.fl@gmail.com>
2024-12-10 14:09:19 -0300

    First Commit

---
+++ AutoSearch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AutoSearch
+{
+    public class AutoSearch
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+        
+        static void Main(string[] args)
+        {
+            MoveMouse(50,50);
+        }
+
+        private static void MoveMouse(int positionX, int positionY)
+        {
+            SetCursorPos(positionX, positionY);
+
+            Console.WriteLine($"Cursor movido para a posição ({positionX}, {positionY})");
+        }
+
+    }
+}
+
