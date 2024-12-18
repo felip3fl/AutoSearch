1669ec5d6f38b86d90e752385e5715e48b2f9cc9
Felip3FL <felip3.fl@gmail.com>
2024-12-17 21:55:31 -0300

    Created new class

---
+++ AutoSearch/Tools/KeyTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSearch.Tools
+{
+    public class KeyTools
+    {
+
+    }
+}
