4f8a5d75d9965dc1dcb4282ded8984e27d8a1af4
Felip3FL <felip3.fl@gmail.com>
2025-01-03 11:47:49 -0300

    Add delay after key actions in KeyTools class

--- Terminal/Tools/KeyTools.cs
+++ Terminal/Tools/KeyTools.cs
@@ -5,21 +5,30 @@
         public void SendCtrlV()
         {
             SendKeys.SendWait("^v");
+            DefaultTimeSleep();
         }
         
         public void SendCtrlA()
         {
             SendKeys.SendWait("^a");
+            DefaultTimeSleep();
         }
         
         public void SendEnter()
         {
             SendKeys.SendWait("{ENTER}");
+            DefaultTimeSleep();
         }
         
         public void SendDelete()
         {
             SendKeys.SendWait("{DELETE}");
+            DefaultTimeSleep();
+        }
+
+        private void DefaultTimeSleep()
+        {
+            System.Threading.Thread.Sleep(500);
         }
     }
 }
