8eca074e1b72dfbc752fa68beb2435ea3fdcbe31
Felip3FL <felip3.fl@gmail.com>
2025-01-06 11:51:11 -0300

    Add tryCatch in Clipboard method

--- Terminal/Tools/ClipboardHelper.cs
+++ Terminal/Tools/ClipboardHelper.cs
@@ -4,9 +4,27 @@ public class ClipboardHelper
 {
     public void SetTextClipboard(string text)
     {
-        var clipboardThread = new Thread(() => Clipboard.SetText(text));
+        var clipboardThread = new Thread(() => ClipboardSetText(text));
         clipboardThread.SetApartmentState(ApartmentState.STA); 
         clipboardThread.Start();
         clipboardThread.Join();
+    }
+
+    private void ClipboardSetText(object text)
+    {
+        try
+        {
+            Clipboard.SetText((string)text);
+        }
+        catch (Exception)
+        {
+            var tryAgainInSeconds = 4;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error on set clipboard. Tring again in {tryAgainInSeconds} seconds");
+            Console.ForegroundColor = default;
+            Thread.Sleep(1000 * tryAgainInSeconds);
+            ClipboardSetText(text);
+        }
+        
     }
 }
