a04fa1b8f83090f564229599a9c1899a6ac7f6c0
Felip3FL <felip3.fl@gmail.com>
2025-01-27 21:08:10 -0300

    2 way to put contructor

--- LocalFile/Models/Record.cs
+++ LocalFile/Models/Record.cs
@@ -2,6 +2,20 @@
 {
     public class Record
     {
+        public Record()
+        {
+
+        }
+
+        public Record(string name, string nameWithExtension, string path, string extension, int size)
+        {
+            Name = name;
+            NameWithExtension = nameWithExtension;
+            Path = path;
+            Extension = extension;
+            Size = size;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string NameWithExtension { get; set; }
