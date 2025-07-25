48113990dc247cf64785e1b216e4c32d2a435c5b
Felip3FL <felip3.fl@gmail.com>
2025-07-25 09:49:42 -0300

    New SettingsConfig file

---
+++ Terminal/Tools/SettingsConfig.cs
@@ -0,0 +1,35 @@
+﻿using AutoSearch.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace AutoSearch.Tools
+{
+    public static class SettingsConfig
+    {
+        
+        public static MousePosition mousePosition;
+        public static ThreadSleep threadSleep;
+
+        public static void LoadConfig()
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var Configuration = builder.Build();
+        }
+
+
+        public static MousePosition GetSetting(string key)
+        {
+
+            //return Configuration[key];
+
+
+            //var setting = Configuration.GetSection("MousePosition").Get<MousePosition>();
+            LoadConfig();
+            return null;
+        }
+    }
+}
