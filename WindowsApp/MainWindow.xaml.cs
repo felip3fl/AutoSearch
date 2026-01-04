6da193a3845fef29531392ba0b2485170a8c4723
Felip3FL <felip3.fl@gmail.com>
2026-01-04 19:39:35 -0300

    New layout autosearch

--- WindowsApp/MainWindow.xaml.cs
+++ WindowsApp/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -7,11 +8,13 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,13 +24,34 @@ namespace WindowsApp
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+
+
     public sealed partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
             ExtendsContentIntoTitleBar = true;
+
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 700, Height = 455 });
+
+            //var presenter = appWindow.Presenter as Microsoft.UI.Windowing.OverlappedPresenter;
+            //if (presenter != null)
+            //{
+            //    presenter.IsResizable = false;
+            //    presenter.IsMaximizable = false;
+            //}
+
+            CenterWindow();
+
         }
+
+
 
         private void btnAddCustomer_Click(object sender, RoutedEventArgs e)
         {
@@ -37,5 +61,15 @@ namespace WindowsApp
         {
 
         }
+
+        private void CenterWindow()
+        {
+            var area = DisplayArea.GetFromWindowId(AppWindow.Id, DisplayAreaFallback.Nearest)?.WorkArea;
+            if (area == null) return;
+            AppWindow.Move(new PointInt32((area.Value.Width - AppWindow.Size.Width) / 2, (area.Value.Height - AppWindow.Size.Height) / 2));
+        }
+
+
     }
+
 }
