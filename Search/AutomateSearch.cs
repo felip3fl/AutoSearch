bda1d26a13b7674e14a08a5e76bcfa2e110bbc2b
Felip3FL <felip3.fl@gmail.com>
2026-01-11 17:53:51 -0300

    New automateSearch class

---
+++ Search/AutomateSearch.cs
@@ -0,0 +1,50 @@
+﻿using Search.Models;
+using System.Text.RegularExpressions;
+
+namespace Search
+{
+    public class AutomateSearch
+    {
+        static List<int> excludedNumbers = new();
+        public string DrawName(ListOfSearch listOfSearch)
+        {
+            Random rnd = new Random();
+
+            int musicIndex = rnd.Next(1, listOfSearch.Name.Count());
+
+            if (CheckExcludedNumbers(musicIndex))
+                return DrawName(listOfSearch);
+
+            AddExcludedNumbers(musicIndex);
+
+            var selectedValue = listOfSearch.Name[musicIndex];
+
+            if (ContainsNonAlphabeticalCharacters(selectedValue))
+                return DrawName(listOfSearch);
+
+            return selectedValue;
+        }
+
+        private static bool CheckExcludedNumbers(int number)
+        {
+            if (excludedNumbers.Contains(number))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool ContainsNonAlphabeticalCharacters(string input)
+        {
+            var isMatch = !Regex.IsMatch(input, @"^[a-zA-Z"",.()!?'\-\s]*$");
+            return isMatch;
+        }
+
+        private static void AddExcludedNumbers(int numberToExclude)
+        {
+            excludedNumbers.Add(numberToExclude);
+        }
+
+    }
+
+}
